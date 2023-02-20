7b18358edb1b01e6e4c785d722f71945c3674597
Beno Sons <re.sonjaya@gmail.com>
2023-02-20 17:10:35 +0700

    assets

---
+++ assets/global/jquery-validate/captcha/captcha.js
@@ -0,0 +1,39 @@
+$(function () {
+			$("body").on("click", "#refreshimg", function () {
+				$.post("newsession");
+				$("#captchaimage").load("image_req");
+				return false;
+			});
+
+			$("#submit_form").validate({
+					rules: {
+
+						captcha1: {
+							required: true,
+							remote: {
+								url: "process",
+								type: "get"
+							}
+
+						}
+					},
+					messages: {
+
+						captcha1: {
+							required: "Please enter a valid Captcha",
+							remote: "Correct captcha is required. Click the captcha to generate a new one"
+
+						},
+						submitHandler: function () {
+							alert("Correct captcha!");
+						},
+						success: function (label) {
+							label.addClass("valid").text("Valid captcha!")
+						},
+						onkeyup: false
+
+
+
+					});
+
+			});
